30897ee20e0b33b508aad7900db3432b27fac325
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-05 11:23:11 +0800

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(news.ssangyongsportas.org)
+domains=(news.ssangyongsports.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ssangyongsports1@gmail.com" # Adding a valid address is strongly recommended
